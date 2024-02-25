4cd0b2aced43b90b2e763b522906827776bf3815
asufana <asufanah@gmail.com>
2024-02-25 16:29:08 +0900

    wip

---
+++ add.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ $# -lt 1 ]; then exit 1; fi
+cargo compete new $1
+cargo member include practice
+
+cd $1
---
+++ submit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+if [ $# -lt 1 ]; then exit 1; fi
+
+cargo compete submit --manifest-path $(pwd)/Cargo.toml $1
