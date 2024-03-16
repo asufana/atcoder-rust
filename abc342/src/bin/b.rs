aefa1e5d6606ab3a95ca924a6860c5ba58de3ba7
asufana <asufanah@gmail.com>
2024-03-16 20:53:56 +0900

    abc342 b

--- abc342/src/bin/b.rs
+++ abc342/src/bin/b.rs
@@ -1,12 +1,31 @@
-use itertools::iproduct;
 use proconio::input;
 
 fn main() {
     input! {
-        a: usize,
-        s: String
+        n: usize,
+        p: [usize; n],
+        q: usize,
+        a: [(usize, usize); q],
     }
 
-    let ret = "";
-    println!("{}", ret);
+    let ps = (1..).zip(p).collect::<Vec<(usize, usize)>>();
+
+    a.iter()
+        .map(|&a| solve(ps.clone(), a))
+        .for_each(|n| println!("{}", n));
+}
+
+fn find(ps: Vec<(usize, usize)>, a: usize) -> (usize, usize) {
+    *ps.iter().find(|&&(_, n)| n == a).unwrap()
+}
+
+fn solve(ps: Vec<(usize, usize)>, a: (usize, usize)) -> usize {
+    let first = find(ps.clone(), a.0);
+    let second = find(ps.clone(), a.1);
+
+    if first.0 < second.0 {
+        first.1
+    } else {
+        second.1
+    }
 }
