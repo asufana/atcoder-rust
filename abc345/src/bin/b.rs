03bada172f92ea5a953c6f2b1b854619b8101b48
asufana <asufanah@gmail.com>
2024-03-20 18:03:41 +0900

    abc345 b

--- abc345/src/bin/b.rs
+++ abc345/src/bin/b.rs
@@ -1,12 +1,13 @@
-use itertools::iproduct;
 use proconio::input;
 
 fn main() {
     input! {
-        a: usize,
-        s: String
+        x: i64
     }
 
-    let ret = "";
+    //切り上げ除算
+    //https://atcoder.jp/contests/abc345/editorial/9562
+    //rustの場合、xが負の場合小数点以下が切り捨てられる
+    let ret = if x >= 0 { (x + 10 - 1) / 10 } else { x / 10 };
     println!("{}", ret);
 }
