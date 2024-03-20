45ea71c28ae4bddabac101bccd22a1c7276ecda3
asufana <asufanah@gmail.com>
2024-03-20 11:30:46 +0900

    abc341 b

--- abc341/src/bin/b.rs
+++ abc341/src/bin/b.rs
@@ -1,12 +1,16 @@
-use itertools::iproduct;
 use proconio::input;
 
 fn main() {
     input! {
-        a: usize,
-        s: String
+        n: usize,
+        a: [usize; n],
+        st: [(usize, usize); n - 1]
     }
 
-    let ret = "";
-    println!("{}", ret);
+    let mut st = st.to_vec();
+    st.push((1, 1));
+    let ast = a.iter().zip(st).collect::<Vec<(&usize, (usize, usize))>>();
+
+    let ret = ast.iter().fold(0, |acc, (&a, (s, t))| (a + acc) / s * t);
+    println!("{:?}", ret);
 }
